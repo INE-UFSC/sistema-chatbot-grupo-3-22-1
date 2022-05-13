9ec26cf084e5deb6770960901a8e2a89daa21260
caioopra <caioprsilva@gmail.com>
2022-05-13 11:09:13 -0300

    BotCansado adicionado ao app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
-from Bots.BotZangado import BotZangado
+from Bots.BotCansado import BotCansado
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotCansado("Yoda", {"1": "Saudação", "2": "Qual seu nome?", "3": "Quero uma dica", "4": "Adeus"})]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
