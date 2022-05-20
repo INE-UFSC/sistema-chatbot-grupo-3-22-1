8571becd49b4f45f0c5f31d4b9a98a1ae351313b
NicolasAvansi <nicolas.avansi@gmail.com>
2022-05-20 10:37:29 -0300

    Enviando alterações bot estudante

--- Bots/BotEstudante.py
+++ Bots/BotEstudante.py
@@ -1,33 +1,19 @@
-from Bots.Bot import Bot
+from Bot import Bot
 from random import choice
 
 class BotEstudante(Bot):
     def __init__(self, nome, comandos):
         super().__init__(nome, comandos)
+        self.__respostasBot = [["Desculpa, não posso, tô atrasado pra aula", "Estudou pra prova hoje?"], ["Me chamo {}, só não me pergunta o meu número de matrícula que eu não vou lembrar".format(self.nome), "Depois da aula de álgebra, nem eu lembro"], ["Hoje não sei, mas a última vez que teve linguiça, eu mordi e ela me disse oink de tão crua que tava", "Não tenho nem tempo pra almoçar, preciso estudar pra prova", "Dor e desespero"], ["Até mais, vou fazer as listas de POO, tem mais umas cinco!", "Mais duas horas de ônibus até a Palhoça"]]
 
     # executada ao mostrar os bots disponiveis
     def apresentacao(self):
         return "Me salva! Tem CTS hoje"
-   
-    def executa_comando(self, cmd):
-        # saudacoes
-        if cmd == "1":
-            lista_saudacoes = ["Desculpa, não posso, tô atrasado pra aula", "Estudou pra prova hoje?"]
-            return choice(lista_saudacoes)
-        
-        # nome do bot
-        elif cmd == "2":
-            return choice(["Me chamo {}, só não me pergunta o meu número de matrícula que eu não vou lembrar".format(self.nome), "Depois da aula de álgebra, nem eu lembro"])
-        
-        # dica
-        elif cmd == "3":
-            return choice(["Hoje não sei, mas a última vez que teve linguiça, eu mordi e ela me disse oink de tão crua que tava", "Não tenho nem tempo pra almoçar, preciso estudar pra prova", "Dor e desespero"])
-        
-        # adeus
-        elif cmd == "4":
-            return choice(["Até mais, vou fazer as listas de POO, tem mais umas cinco!", "Mais duas horas de ônibus até a Palhoça"])
 
-        else:
+    def executa_comando(self, cmd):
+        try:
+            return(choice(self.__respostasBot[int(cmd)-1]))
+        except:
             return "Me desculpa, mas eu entendi o que tu quis dizer tanto quanto entendi a matéria de sistemas"
 
     # quando bot é escolhido
@@ -37,3 +23,7 @@ class BotEstudante(Bot):
     # chama a funcao despedida quando usa comando -1
     def despedida(self):
         return "Finalmente isso acabou, vou poder comer no RU"
+
+    def adicionar_resposta(self, nomeComando:str, novaResposta: list):
+        self.comandos[str(len(self.comandos)+1)] = nomeComando
+        self.__respostasBot.append(novaResposta)
--- app.py
+++ app.py
@@ -3,8 +3,11 @@ from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotCansado import BotCansado
 from Bots.BotEstudante import BotEstudante
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotCansado("Forever Alone", {"1": "Saudação", "2": "Qual seu nome?", "3": "Quero uma dica", "4": "Adeus"}), BotEstudante("Estudante", {"1": "Cumprimentar", "2": "Qual seu nome?", "3": "O que tem de almoço no RU hoje?", "4": "Até mais"})]
+estudante = BotEstudante("Estudante", {"1": "Cumprimentar", "2": "Qual seu nome?", "3": "O que tem de almoço no RU hoje?", "4": "Até mais"})
 
+lista_bots = [BotCansado("Forever Alone", {"1": "Saudação", "2": "Qual seu nome?", "3": "Quero uma dica", "4": "Adeus"}), estudante]
+
+estudante.adicionar_resposta("Testando", ["teste1", "teste2"])
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
