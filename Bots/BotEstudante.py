be5d1a2067926cab95293dda673f31ca831c341d
NicolasAvansi <nicolas.avansi@gmail.com>
2022-05-20 09:46:24 -0300

    Enviando Bot estudante e implementando no app.py

--- Bots/BotEstudante.py
+++ Bots/BotEstudante.py
@@ -1,28 +1,39 @@
 from Bots.Bot import Bot
+from random import choice
 
 class BotEstudante(Bot):
-    def __init__(self,nome):
-        self.__nome = nome
+    def __init__(self, nome, comandos):
+        super().__init__(nome, comandos)
 
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
+    # executada ao mostrar os bots disponiveis
     def apresentacao(self):
-        return "Me salva, tem CTS hoje!"
+        return "Me salva! Tem CTS hoje"
+   
+    def executa_comando(self, cmd):
+        # saudacoes
+        if cmd == "1":
+            lista_saudacoes = ["Desculpa, não posso, tô atrasado pra aula", "Estudou pra prova hoje?"]
+            return choice(lista_saudacoes)
+        
+        # nome do bot
+        elif cmd == "2":
+            return choice(["Me chamo {}, só não me pergunta o meu número de matrícula que eu não vou lembrar".format(self.nome), "Depois da aula de álgebra, nem eu lembro"])
+        
+        # dica
+        elif cmd == "3":
+            return choice(["Hoje não sei, mas a última vez que teve linguiça, eu mordi e ela me disse oink de tão crua que tava", "Não tenho nem tempo pra almoçar, preciso estudar pra prova", "Dor e desespero"])
+        
+        # adeus
+        elif cmd == "4":
+            return choice(["Até mais, vou fazer as listas de POO, tem mais umas cinco!", "Mais duas horas de ônibus até a Palhoça"])
 
-    def mostra_comandos(self):
-        pass
-    
-    def executa_comando(self,cmd):
-        pass
+        else:
+            return "Me desculpa, mas eu entendi o que tu quis dizer tanto quanto entendi a matéria de sistemas"
 
+    # quando bot é escolhido
     def boas_vindas(self):
-        pass
-
+        return "Opa, só fala rápido que eu tenho que estudar pra cálculo"
+    
+    # chama a funcao despedida quando usa comando -1
     def despedida(self):
-        pass
+        return "Finalmente isso acabou, vou poder comer no RU"
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 #encoding: utf-8
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotCansado import BotCansado
-
+from Bots.BotEstudante import BotEstudante
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotCansado("Forever Alone", {"1": "Saudação", "2": "Qual seu nome?", "3": "Quero uma dica", "4": "Adeus"})]
+lista_bots = [BotCansado("Forever Alone", {"1": "Saudação", "2": "Qual seu nome?", "3": "Quero uma dica", "4": "Adeus"}), BotEstudante("Estudante", {"1": "Cumprimentar", "2": "Qual seu nome?", "3": "O que tem de almoço no RU hoje?", "4": "Até mais"})]
+
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
